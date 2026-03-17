e1bd4c98c30f37f13144da1453732c7609781267
Tasi <tasi87@gmail.com>
2026-03-17 15:33:10 +0100

    lekcia 38

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 function CreateWizard(firstName, mySpell) {
+	// console.log("this: ", this)		// prázdny object
 	this.name 	= firstName
 	this.spell	= mySpell
+	// console.log("this: ", this)		// naplnený object
 }
 
 CreateWizard.prototype.attack = function () {
@@ -8,7 +10,15 @@ CreateWizard.prototype.attack = function () {
 	}
 
 const wizard1 = new CreateWizard("David", "abraka")
-console.log(wizard1.attack())
-
 const wizard2 = new CreateWizard("Harry", "imobilus")
-console.log(wizard2.attack())
+
+// console.log(wizard1.__proto__)
+// console.log(wizard1.prototype)
+// console.log(CreateWizard.prototype)
+
+// console.log(wizard1.__proto__ === CreateWizard.prototype)	// true
+
+// Iba funkcie majú prototype
+console.log(wizard1.prototype)		// je to object a nie funkcia
+console.log(wizard2.prototype)		// je to object a nie funkcia
+console.log(CreateWizard.prototype) // toto je funkcia
